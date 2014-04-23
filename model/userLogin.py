2b41997ea445506a085c3259c163d8110ff40b04
AdriaB <adria_bier@hotmail.com>
2014-04-23 13:55:51 +0200

    Creada la classe "userLogin", els mètodes encara estan per fer

---
+++ model/userLogin.py
@@ -0,0 +1,37 @@
+__author__ = 'adria'
+
+#!/usr/bin/python
+class UserLogin:
+    def __init__(self, usuari, rutafitxer):
+        self.usuari = usuari
+        self.rutafitxer = rutafitxer
+
+
+
+    def comprovaFitxer(self):
+        """Comprova que existeixi el fitxer on es guarden els usuaris"""
+        pass
+
+    def creaFitxer(self):
+        """Crea el fitxer en la ruta 'rutafitxer' """
+        pass
+
+    def llegeixLog(self):
+        """Llegeix el log sencer"""
+        pass
+
+    def llistaUsuaris(self):
+        """Llista tots els usuaris registrats"""
+        pass
+
+    def existeixUsuari(self):
+        """Comprova que existeixi l'usuari actual (self.usuari)"""
+        pass
+
+    def guardaUsuari(self):
+        """Guarda en el fitxer l'usuari actual (self.usuari)"""
+        pass
+
+    def validaContrasenya(self, contrasenya):
+        """Comprova la contrasenya de l'usuari"""
+        pass
