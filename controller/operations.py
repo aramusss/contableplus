adc58c8e08f6e95f8888efa39338618ae1a190ef
kenshyn <victormonell@hotmail.com>
2014-04-25 16:54:11 +0200

    Operations with currencies+currencies

--- controller/operations.py
+++ controller/operations.py
@@ -5,6 +5,8 @@ converter=converter()
 owner=1
 owner_money=3456
 owner_curr='EUR'
+receiver_money=1424
+receiver_curr='EUR'
 boolean=True
 print('Your local currency is set to '+owner_curr+ ' do you want to change it?')
 a=input('Y/N')
@@ -15,4 +17,7 @@ while(boolean):
     boolean=converter.set_locale(owner_curr)
 
 
-converter.convert_from_locale(owner_money)
+
+converter.set_locale(receiver_curr)
+owner_money=converter.convert_to_locale(owner_money)
+receiver_money=owner_money+receiver_money
