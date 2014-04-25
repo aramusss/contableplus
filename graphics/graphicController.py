82f33659836d8d9705d10ea6f63e87f19e6a5b91
aram <aramiquelmateu@gmail.com>
2014-04-25 15:55:32 +0200

    graphics.py

--- graphics/graphicController.py
+++ graphics/graphicController.py
@@ -1,6 +1,14 @@
 __author__ = 'aram'
 
+from graphics import *
+
 #DNI;IBAN;DATE(24/02/2014);+/-IMPORT
 
-
-with open
+class GraphicController:
+    def showGrapicWithDNI(self, inputDni):
+        win =  GraphWin
+        filePath = ""
+        with open(filePath, 'r') as logDB:
+            for line in logDB:
+                    dni, iban, date, money = line.split(";")
+                    if(dni == inputDni):
