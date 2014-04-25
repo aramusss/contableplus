68cf0db93ff29b5ff2804002c59735183f683d24
nejogaro <nejogaro@gmail.com>
2014-04-25 17:02:03 +0200

    account

--- model/account.py
+++ model/account.py
@@ -8,3 +8,30 @@ class Account:
     def muestraDatos(self):
         return [self.iban, self.titular, self.moneda]
 
+    @property
+    def iban(self):
+        return self.iban
+
+    @iban.setter
+    def iban(self, value):
+        self.iban = value
+
+
+    @property
+    def titular(self):
+        return self.titular
+
+    @titular.setter
+    def titular(self, value):
+        self.titular = value
+
+
+    @property
+    def moneda(self):
+        return self.moneda
+
+    @moneda.setter
+    def moneda(self, value):
+        self.moneda = value
+
+
