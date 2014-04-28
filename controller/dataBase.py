2e02e8bed0df2892708e9419a2f210c97bb03b1b
AdriaB <adria_bier@hotmail.com>
2014-04-28 20:00:46 +0200

    Modificat el dataBase perque pugui llegir i afegir comptes noves

--- controller/dataBase.py
+++ controller/dataBase.py
@@ -9,6 +9,7 @@ class DataBase:
         self.rutaUsers = rutaUsers
         self.rutaComptes = rutaComptes
 
+    #User management methods:
     def creaUsers(self):
         """Creates users file"""
         if self.comprovaUsers():
@@ -16,24 +17,10 @@ class DataBase:
         else:
             open(self.rutaUsers, 'w').close()
 
-    def creaComptes(self):
-        """Creates accounts file"""
-        if self.comprovaComptes():
-            print("Accounts file already exists!")
-        else:
-            open(self.rutaComptes, 'w').close()
-
     def comprovaUsers(self):
         """Comprova que existeixi el fitxer on es guarden els usuaris"""
         result = False
         if os.path.isfile(self.rutaUsers):
-            result = True
-        return result
-
-    def comprovaComptes(self):
-        """Comprova que existeixi el fitxer on es guarden les comptes bancaries"""
-        result = False
-        if os.path.isfile(self.rutaComptes):
             result = True
         return result
 
@@ -58,17 +45,6 @@ class DataBase:
         llista.pop()
         #fer alguna cosa si no troba usuaris?
         return llista
-
-    def llegeixComptes(self):
-        """Llegeix tot el fitxer de comptes"""
-        resultat = ""
-        if self.comprovaComptes():
-            with open(self.rutaComptes, 'r') as f:
-                resultat = f.read()
-        else:
-            print("Error! no s'ha trobat el fitxer de comptes")
-            #llençar una excepcio
-        return resultat
 
     def afegeixUsuari(self, dni, nombre, apellidos):
         """Afegeix un usuari al fitxer d'usuaris sense comprovar que existeixi"""
@@ -102,3 +78,75 @@ class DataBase:
                 #error
         else:
             print("Error! no s'ha trobat el fitxer")
+
+    #Account management methods:
+    def creaComptes(self):
+        """Creates accounts file"""
+        if self.comprovaComptes():
+            print("Accounts file already exists!")
+        else:
+            open(self.rutaComptes, 'w').close()
+
+    def comprovaComptes(self):
+        """Checks if the accounts' file (self.rutaComptes) exists"""
+        result = False
+        if os.path.isfile(self.rutaComptes):
+            result = True
+        return result
+
+    def llegeixComptes(self):
+        """Reads all the accounts' file"""
+        resultat = ""
+        if self.comprovaComptes():
+            with open(self.rutaComptes, 'r') as f:
+                resultat = f.read()
+        else:
+            print("Error! no s'ha trobat el fitxer de comptes")
+            #llençar una excepcio
+        return resultat
+
+    def llistaComptes(self):
+        """Returns a list of all the accounts"""
+        list = []
+        text = self.llegeixComptes()
+        listbylines = text.split("\n")
+        for linia in listbylines:
+            list.append(linia.split(","))
+        if len(list) > 0:
+            list.pop()
+        return list
+
+    def llistaIBAN(self):
+        """Returns a list of all the IBAN codes of the bank accounts"""
+        list = []
+        accounts = self.llistaComptes()
+        for a in accounts:
+            list.append(a[0])
+        return list
+
+    def afegeixCompta(self, iban, balance, currency, *owners):
+        """Adds a new bank account, all filds are mandatory"""
+        #comprovar que tots els owners existeixin?
+        added = False
+        ibanExists = False
+        if self.comprovaComptes():
+            ibanList = self.llistaIBAN()
+            for i in ibanList:
+                if i == iban:
+                    ibanExists = True
+            if not ibanExists:
+                if iban and balance and currency and (len(owners) > 0) and ("" not in owners) and (None not in owners):
+                    ownerslist = ""
+                    for o in owners:
+                        ownerslist += o + ","
+                    ownerslist = ownerslist[:len(ownerslist)-1]
+                    with open(self.rutaComptes, 'a') as f:
+                        f.write(iban+","+balance+","+currency+","+"".join(ownerslist)+"\n")
+                        added = True
+                else:
+                    print("Error, a bank account can't have any empty fields")
+            else:
+                print("Error: bank account could not be added: IBAN code already exists")
+        else:
+            print("Error: could not find bank accounts' file")
+        return added
