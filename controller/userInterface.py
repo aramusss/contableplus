6a33be6a75a4f7409d33fe519d738e98cb2d5200
kenshyn <victormonell@hotmail.com>
2014-05-08 16:52:23 +0200

    Aram Marica

--- controller/userInterface.py
+++ controller/userInterface.py
@@ -2,7 +2,7 @@ __author__ = 'aram'
 
 from userLogin import *
 
-#This class will be the one that controlls
+#This class will be the one that controlleixions
 #the inputs and outputs for the user interface.
 
 print("Hello Worker! Welcome to ContablePlus!")
